32698a07be9067e3ea3204175de4533dc6ceba2d
Warrior-Player <@Ayoub-Marzouki>
2025-10-23 16:17:06 +0100

    Added a basic front to the app

---
+++ src/main/java/ai/autoapply/web/controllers/WebController.java
@@ -0,0 +1,20 @@
+package ai.autoapply.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class WebController {
+    
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("start")
+    public String landing() {
+        return "start";
+    }
+}
